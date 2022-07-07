646c28221b9f9b1795e60464e9d39daaff84a770
kc0813 <katherinechan4@gmail.com>
2022-07-07 04:01:29 -0400

    feat: added a couple of new todo functions and wrote read emails

--- commands.py
+++ commands.py
@@ -1,7 +1,10 @@
 import os
+import imaplib
+import email
 from unicodedata import name
 import requests
 from dotenv import load_dotenv
+from glados import glados_speak
 
 load_dotenv("secrets.env")
 wkey = os.getenv("WEATHER_KEY")
@@ -19,7 +22,7 @@ def fetchWeather(loc: str):
         lon = os.getenv("HOME_LON")
     else:
         raise ValueError("Invalid location")
-    url = "https://api.openweathermap.org/data/3.0/onecall?lat={}&lon={}&exclude=minutely,hourly,alerts&appid={}".format(
+    url = "https://api.openweathermap.org/data/2.5/onecall?lat={}&lon={}&exclude=minutely,hourly,alerts&units=imperial&appid={}".format(
         lat, lon, wkey
     )
     response = requests.get(url)
@@ -33,15 +36,53 @@ def fetchWeather(loc: str):
     except KeyError as e:
         raise ValueError("Invalid response from API") from e
 
-    ret = """It is currently {} degrees but feels like {} The weather is {}. The high today is {} and the low is {}.
-             You would know that if you went outside and actually touched grass.""".format(
+    ret = "It is currently {} degrees and feels like {} The weather is {}. The high today is {} and the low is {}. You would know that if you went outside and actually touched grass.".format(
         temp, feelslike, weather, maxTemp, minTemp
     )
-    return ret
+
+    glados_speak(ret)
 
 
 def toggleLight():
     pass
+
+
+def nextClass():
+    pass
+
+
+def editClass():
+    pass
+
+
+def readEmails():
+    """
+    Reads unread emails from the inbox.
+    """
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(os.getenv("EMAIL_USER"), os.getenv("EMAIL_PASS"))
+    mail.select("inbox")
+    _, data = mail.search(None, "UNSEEN")
+    ids = data[0]
+    id_list = ids.split()
+    for i in id_list:
+        _, data = mail.fetch(i, "(RFC822)")
+        raw = data[0][1]
+        msg = email.message_from_bytes(raw)
+        glados_speak(
+            "email from {} about {}. Would you like to read the email?".format(
+                msg["from"], msg["subject"]
+            )
+        )
+        # TODO: Read the email and respond accordingly
+        answer = input("y/n: ")
+        if answer == "y":
+            glados_speak(msg.get_payload())
+            mail.store(i, "+FLAGS", "\\Seen")
+
+    glados_speak(
+        "Oh you've got one more email and its from me. Want to know what it says? ... You are a horrible person. I'm serious, that's what it says: A horrible person. We weren't even testing for that."
+    )
 
 
 if __name__ == "__main__":
--- glados.py
+++ glados.py
@@ -3,6 +3,7 @@ from utils.tools import prepare_text
 from scipy.io.wavfile import write
 import time
 from sys import modules as mod
+
 try:
     import winsound
 except ImportError:
@@ -12,27 +13,56 @@ print("Initializing TTS Engine...")
 
 # Select the device
 if torch.is_vulkan_available():
-    device = 'vulkan'
+    device = "vulkan"
 if torch.cuda.is_available():
-    device = 'cuda'
+    device = "cuda"
 else:
-    device = 'cpu'
+    device = "cpu"
 
 # Load models
-glados = torch.jit.load('models/glados.pt')
-vocoder = torch.jit.load('models/vocoder-gpu.pt', map_location=device)
+glados = torch.jit.load("models/glados.pt")
+vocoder = torch.jit.load("models/vocoder-gpu.pt", map_location=device)
 
 # Prepare models in RAM
 for i in range(4):
     init = glados.generate_jit(prepare_text(str(i)))
-    init_mel = init['mel_post'].to(device)
+    init_mel = init["mel_post"].to(device)
     init_vo = vocoder(init_mel)
 
-while(1):
+
+def glados_speak(text):
+    """
+    generates audio from text and plays it
+    """
+    # Generate generic TTS-output
+    tts_output = glados.generate_jit(x)
+
+    # Use HiFiGAN as vocoder to make output sound like GLaDOS
+    mel = tts_output["mel_post"].to(device)
+    audio = vocoder(mel)
+
+    # Normalize audio to fit in wav-file
+    audio = audio.squeeze()
+    audio = audio * 32768.0
+    audio = audio.cpu().numpy().astype("int16")
+    output_file = "output.wav"
+
+    # Write audio file to disk
+    # 22,05 kHz sample rate
+    write(output_file, 22050, audio)
+
+    # Play audio file
+    if "winsound" in mod:
+        winsound.PlaySound(output_file, winsound.SND_FILENAME)
+    else:
+        call(["aplay", "./output.wav"])
+
+
+while 1:
     text = input("Input: ")
 
     # Tokenize, clean and phonemize input text
-    x = prepare_text(text).to('cpu')
+    x = prepare_text(text).to("cpu")
 
     with torch.no_grad():
 
@@ -43,22 +73,22 @@ while(1):
 
         # Use HiFiGAN as vocoder to make output sound like GLaDOS
         old_time = time.time()
-        mel = tts_output['mel_post'].to(device)
+        mel = tts_output["mel_post"].to(device)
         audio = vocoder(mel)
         print("HiFiGAN took " + str((time.time() - old_time) * 1000) + "ms")
-        
+
         # Normalize audio to fit in wav-file
         audio = audio.squeeze()
         audio = audio * 32768.0
-        audio = audio.cpu().numpy().astype('int16')
-        output_file = ('output.wav')
-        
+        audio = audio.cpu().numpy().astype("int16")
+        output_file = "output.wav"
+
         # Write audio file to disk
         # 22,05 kHz sample rate
         write(output_file, 22050, audio)
 
         # Play audio file
-        if 'winsound' in mod:
+        if "winsound" in mod:
             winsound.PlaySound(output_file, winsound.SND_FILENAME)
         else:
             call(["aplay", "./output.wav"])
